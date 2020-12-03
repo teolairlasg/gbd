8f9ef65613749b2cb0ee3fb401605fd840080463
Teo <teo.lairla@iessierradeguara.com>
2020-12-03 16:35:48 +0100

    primeros pasos js

---
+++ web01-js/app.js
@@ -0,0 +1,23 @@
+console.log("Hola mundo");
+
+let texto = "Esto es un texto";
+let numero = 25;
+let decimal = 5.4;
+let booleano = true;
+
+let personaArray = ["Jose", 58, "Moreno"];
+
+let persona = {
+    "nombre": "Manuel",
+    "edad": 23,
+    "cabello": "Rubio"
+}
+
+console.log(texto);
+console.log(numero);
+console.log(decimal);
+console.log(booleano);
+console.log(personaArray)
+console.log(persona.nombre);
+console.log(persona["nombre"]);
+console.log(personaArray[1]);
